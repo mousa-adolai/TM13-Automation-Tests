fbeb05144d783445fd55c2b2be893bf418b81eb9
edumorris <62588003+edumorris@users.noreply.github.com>
2021-09-15 22:10:41 +0300

    Test base utilities added.

---
+++ src/test/java/tests/utility/TestBase.java
@@ -0,0 +1,25 @@
+package tests.utility;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.openqa.selenium.WebDriver;
+
+import java.util.concurrent.TimeUnit;
+
+public class TestBase {
+    protected static WebDriver driver;
+
+    // Setting up driver
+    @BeforeEach
+    public void driverSetup() {
+        driver = WebDriverFactory.getDriver("chrome"); // Change parameter to the browser you require
+
+        // Implicit wait to wait for elements to be found on a loading page before throwing an error
+        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+    }
+
+    @AfterEach
+    public void closeBrowser() {
+        driver.quit();
+    }
+}
---
+++ src/test/java/tests/utility/WebDriverFactory.java
@@ -0,0 +1,38 @@
+package tests.utility;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.edge.EdgeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.opera.OperaDriver;
+
+public class WebDriverFactory {
+    public static WebDriver getDriver(String browserName) {
+        WebDriver driver;
+        
+        switch (browserName.toLowerCase()) {
+            case "chrome":
+                WebDriverManager.chromedriver().setup();
+                driver = new ChromeDriver();
+                break;
+            case "firefox":
+                WebDriverManager.firefoxdriver().setup();
+                driver = new FirefoxDriver();
+                break;
+            case "edge":
+                WebDriverManager.edgedriver().setup();
+                driver = new EdgeDriver();
+                break;
+            case "opera":
+                WebDriverManager.operadriver().setup();
+                driver = new OperaDriver();
+                break;
+            default:
+                driver = null;
+                System.err.println("Unknown browser type " + browserName);
+        }
+
+        return driver;
+    }
+}
