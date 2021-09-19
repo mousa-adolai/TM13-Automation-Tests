5154a83752444451258d19aa81e978bede61d613
Mousa Adolai <mousa.adolai@gmail.com>
2021-09-19 15:51:02 -0400

    completed test script for user story 2, test case 1 -- Librarian adds new user.

--- src/test/java/com/libraryCT/mousa/user_story_2_add_new_user_function/T1LibrarianAddsNewUser.java
+++ src/test/java/com/libraryCT/mousa/user_story_2_add_new_user_function/T1LibrarianAddsNewUser.java
@@ -1,32 +1,50 @@
 package com.libraryCT.mousa.user_story_2_add_new_user_function;
 
 import com.libraryCT.utility.TestBase;
-import io.github.bonigarcia.wdm.WebDriverManager;
 import org.junit.jupiter.api.Test;
 import org.openqa.selenium.By;
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.edge.EdgeDriver;
+import org.openqa.selenium.WebElement;
+
+import static org.junit.jupiter.api.Assertions.*;
 
 public class T1LibrarianAddsNewUser extends TestBase {
+    /*
+    AC #1:
+    Given librarian is on the home page
+    When librarian click Users module
+    And librarian click “+Add User” button
+    When librarian enter full name, password, email and address
+    And librarian click save changes
+    Then verify a new user is created
+     */
 
-   @Test //calls the Test interface
-   public void librarianAddsNewUser(){
-
-       //locate and send valid email to email input bar
+    @Test //calls the Test interface
+    public void librarianAddsNewUser() {
+        //go to QA env
+        driver.get("https://library2.cybertekschool.com/");
+        //locate and send valid email to email input bar
         driver.findElement(By.id("inputEmail")).sendKeys("librarian21@library");
-       //locate and send valid email to password input bar
+        //locate and send valid email to password input bar
         driver.findElement(By.xpath("//*[@id=\"inputPassword\"]")).sendKeys("Sdet2022*");
         //press sign in button
         driver.findElement(By.cssSelector("button.btn.btn-lg.btn-primary.btn-block")).click();
-       //sign in complete, librarian is on homepage
+        //sign in complete, librarian is on homepage
 
-
-
-        driver.findElement(By.xpath("//a[@class='btn btn-lg btn-outline btn-primary btn-sm']")).click();//add user button
+        //librarian clicks users module
+        driver.findElement(By.linkText("Users")).click();
+        //librarian clicks add user button
+        driver.findElement(By.xpath("//a[@class='btn btn-lg btn-outline btn-primary btn-sm']")).click();
+        //librarian fills out the add new user form
         driver.findElement(By.xpath("//*[@id=\"add_user_form\"]/div[1]/div/div[1]/div/div/input")).sendKeys("Rahel Legese");//full name field
         driver.findElement(By.xpath("//*[@id=\"add_user_form\"]/div[1]/div/div[2]/div[1]/div/input")).sendKeys("Ilovecybertek");//password field
-        driver.findElement(By.xpath("//*[@id=\"add_user_form\"]/div[1]/div/div[2]/div[2]/div/input")).sendKeys("Ilovecybertek@cybertekschool.com");//first name field
+        driver.findElement(By.xpath("//*[@id=\"add_user_form\"]/div[1]/div/div[2]/div[2]/div/input")).sendKeys("Ilovecybertek@cybertekschool3.com");//first name field
+        //librarian submits add new user form
         driver.findElement(By.xpath("//*[@id=\"add_user_form\"]/div[2]/button[2]")).submit();//submit button
+
+        //locate the most recently added user
+        WebElement mostRecentUserName = driver.findElement(By.xpath("//table[@id = 'tbl_users']/tbody/tr[1]/td[3]"));
+        //verify the new user has been added by checking the actual first name matches the expected
+        assertEquals("Rahel Legese", mostRecentUserName.getText());
 
 
     }
